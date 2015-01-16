8231af1db73aa7314fbc24cba97755d494d2f903
Kasper Frank <kasperdaff@gmail.com>
2015-01-16 13:52:18 +0100

    Added files

---
+++ EmbeddedSQLite/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Web;
+using Pronestor.EmbeddedSQLite;
+
+[assembly: PreApplicationStartMethod(typeof(SqLiteAssemblyLoader), "Init")]
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EmbeddedSQLite")]
+[assembly: AssemblyDescription("Embeds x64 and x86 System.Data.SQLite.dlls and resolves the correct one depending on process bitness")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Pronestor")]
+[assembly: AssemblyProduct("EmbeddedSQLite")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0b49491d-b586-4866-894b-f0ba70be1bcc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EmbeddedSQLite/SQLiteAssemblyLoader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Web.Hosting;
+
+namespace Pronestor.EmbeddedSQLite
+{
+  public static class SqLiteAssemblyLoader
+  {
+    private static readonly Lazy<Assembly> _sqlLiteAssemblyLoader;
+
+    static SqLiteAssemblyLoader()
+    {
+      _sqlLiteAssemblyLoader = new Lazy<Assembly>(() =>
+      {
+        bool is64BitProcess = (IntPtr.Size == 8);
+        const string dllName = "System.Data.SQLite.DLL";
+        
+        // Embedded dll resource names:
+        // 32-bit: EmbeddedSQLite.Resources.x86.System.Data.SQLite.DLL
+        // 64-bit: EmbeddedSQLite.Resources.x64.System.Data.SQLite.DLL
+
+        string embeddedDllResourceName = "Pronestor.EmbeddedSQLite.Resources." + (is64BitProcess ?  "x64" : "x86") + "." + dllName;
+        using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(embeddedDllResourceName))
+        {
+          string filePath = Path.Combine(GetDllDirectory(), dllName);
+          using (Stream fileStream = File.OpenWrite(filePath))
+          {
+            stream.CopyTo(fileStream);
+            fileStream.Flush();
+          }
+
+          Assembly asm = Assembly.LoadFile(filePath);
+          return asm;
+        }
+      });
+    }
+
+    public static void Init()
+    {
+      AppDomain.CurrentDomain.AssemblyResolve += (sender, args) => 
+        args.Name == "System.Data.SQLite, Version=1.0.61.0, Culture=neutral, PublicKeyToken=db937bc2d44ff139" ? _sqlLiteAssemblyLoader.Value : null;
+    }
+
+    private static string GetDllDirectory()
+    {
+      return HostingEnvironment.IsHosted ? Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "App_Data") : Directory.GetCurrentDirectory();
+    }
+  }
+}
